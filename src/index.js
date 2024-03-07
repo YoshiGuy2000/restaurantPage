4e338878d430452b33658dda151dcc99194b2087
YoshiGuy2000 <danielavilalearns@gmail.com>
2024-03-07 18:02:51 -0500

    Push initial files to github

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// import generateJoke from "./generateJoke.js"
+// import './styles/main.css'
+// import laughing from './assets/laughing.jpeg'
+
+// const laughImg = document.getElementById("laughImg")
+// laughImg.src = laughing;
+
+// const jokeBTN = document.getElementById("jokeBtn");
+// jokeBTN.addEventListener("click", generateJoke)
+
+// generateJoke()
+// console.log("This is working now!")
+
+import './styles/main.css'
+
+console.log("Testing server updates")
